c4e4f60c577d2c60ff856530f938d6c5e5524ec0
Vladyslav Korol <95221193+ChocolateMagnate@users.noreply.github.com>
2022-08-17 08:54:03 +0000

    added package details.

---
+++ Chrome/background.js
@@ -0,0 +1,9 @@
+chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+
+    // since only one tab should be active and in the current window at once
+    // the return variable should only have one entry
+    var activeTab = tabs[0];
+    var activeTabId = activeTab.id; // or do whatever you need
+    console.log(activeTabId);
+
+ });
--- Chrome/process.ts
+++ Chrome/process.ts
@@ -1,3 +1,22 @@
+
+function verify(): void { //Just to make sure the extension is running.
+    console.log("Extension works!");
+}
+/** Class that represents the assiting lookup. */
+class Assistant {
+    constructor(public word: string, public language: string) {}
+    definitions = fetch(`https://www.dict.com/${this.language}-english/${this.word}`)
+        .then(response => response.text())
+    
+    public View(): void {
+        console.log(this.definitions);
+    }
+}
+/*
+
+function fetchFromDisctionary(word: string, language: string) {}
+
+
 function assist(word: string, language: string): void {
     //The core of the extension. Looks up the given word in dictionary and displays its transation, grammar.
     let dictionary = 0;
@@ -20,3 +39,4 @@ function assist(word: string, language: string): void {
     // Execute
     Do();
 }
+*/
